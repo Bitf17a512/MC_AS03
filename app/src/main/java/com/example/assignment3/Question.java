857f14c117a08f8cfc800b72cc2fe61693bae792
bitf17a512 <bitf17a512@pucit.edu.pk>
2021-11-19 15:20:45 +0500

    Get Choice Class

--- app/src/main/java/com/example/assignment3/Question.java
+++ app/src/main/java/com/example/assignment3/Question.java
@@ -111,3 +111,7 @@ public class Question {
         String question = questions[a];
         return question;
     }
+    public String getchoice1(int a){
+        String choice = choices[a][0];
+        return choice;
+    }
