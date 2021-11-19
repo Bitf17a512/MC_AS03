730778e6f52d62f9f27f1a91529cf845e5139ba2
bitf17a512 <bitf17a512@pucit.edu.pk>
2021-11-19 15:00:18 +0500

    Question Class

--- app/src/main/java/com/example/assignment3/MainActivity.java
+++ app/src/main/java/com/example/assignment3/MainActivity.java
@@ -1,14 +1,27 @@
-package com.example.assignment3;
+package com.example.quizapp;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-}
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button btn_one, btn_two, btn_three, btn_four;
+    TextView tv_question;
+    public int score = 0;
+
+
+
+    private Question question = new Question();
+
+    private String answer;
+    private int questionLength = question.questions.length;
