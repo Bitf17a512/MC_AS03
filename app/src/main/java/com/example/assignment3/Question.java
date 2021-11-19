48c4d65fd55477c25af8d1566d808542f209635a
bitf17a512 <bitf17a512@pucit.edu.pk>
2021-11-19 15:15:46 +0500

    Question add

--- app/src/main/java/com/example/assignment3/Question.java
+++ app/src/main/java/com/example/assignment3/Question.java
@@ -1,4 +1,11 @@
-package com.example.assignment3;
+package com.example.quizapp;
 
 public class Question {
-}
+
+
+    public String questions[] = {
+            "Where is the sound of 'أ' produced from?",
+            "Where is the sound of 'ہ' produced from?",
+            "Where is the sound of 'ح' produced from?",
+            "Where is the sound of 'ع' produced from?",
+            "Where is the sound of 'غ' produced from?",
